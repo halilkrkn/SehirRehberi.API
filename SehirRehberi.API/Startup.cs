c50200d97db97b6c680364bf5c6a3e9aa65457f2
halilkrkn <Halilkrkn000@gmail.com>
2019-07-22 18:20:49 +0300

    Revert "Revert "Initial commit""
    This reverts commit 13e1cd877b21f7b29eaa853d1c9f3599479388fa.

---
+++ SehirRehberi.API/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SehirRehberi.API.Data;
+
+namespace SehirRehberi.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+       
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("Appsettings:Token").Value);
+
+            services.AddDbContext<DataContext>(x =>
+            x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                
+                //  ReferenceLoop ile karılaşırsak onu onu ignore ettik.
+                // Yani burda veri tabanında ilişkilendirdiğimiz kısımlar da aynı olanlar tekrrdan gözükmemesini sağladık
+                .AddJsonOptions(opt => {
+
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+                    //Uygulamada Authentication mimarisini kullancaz. BUnu JWT konfigurasyonu ile yapacaz 
+                    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(key),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                        };
+
+                    });
+
+            });
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            services.AddAutoMapper();
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            //Cors Konfigurasyonu 
+            services.AddCors();
+
+            //Burda depencing İnjection yapıldı.
+            // Yani bir controller eğer bizden IAppRepository isterse ona AppRepository i karşılığı demek istedik
+            services.AddScoped<IAppRepository, AppRepository>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+
+            
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //Tarayıcılara Güvenli Olduğunu belirtmek için kullanılır.
+            app.UseCors(x=> x.AllowAnyHeader().AllowAnyHeader().AllowAnyOrigin().AllowCredentials());
+
+            app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
